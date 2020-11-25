88cf697006a3cff1533902e74d7c51e61910428c
Luis Vargas <72893367+luisv99@users.noreply.github.com>
2020-11-25 19:15:57 -0400

    es luis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,12 +27,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DetalleProductoComponent } from './Components/detalle-producto/detalle-producto.component';
 import { VistaUsersComponent } from './Components/vista-users/vista-users.component';
-<<<<<<< HEAD
-=======
 import { FilterPipe } from './pipes/filter.pipe';
 import { CategoriasComponent } from './Components/categorias/categorias.component';
-import { RegistroComponent } from './components/registro/registro.component';
->>>>>>> 42872637c44a3eb7b3db66f5eecb8ff17a82ccf5
 import { LoginComponent } from './components/login/login.component';
 
 
@@ -51,14 +47,10 @@ import { LoginComponent } from './components/login/login.component';
     AddProductComponent,
     DetalleProductoComponent,
     VistaUsersComponent,
-<<<<<<< HEAD
     LoginComponent,
-=======
     FilterPipe,
     CategoriasComponent, 
-    RegistroComponent,
     LoginComponent
->>>>>>> 42872637c44a3eb7b3db66f5eecb8ff17a82ccf5
   ],
   imports: [
     BrowserModule,
