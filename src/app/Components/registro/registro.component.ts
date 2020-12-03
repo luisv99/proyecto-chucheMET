2ac9c3addd73d9e1941e3f607e0a8778fc0ab560
Luis Vargas <72893367+luisv99@users.noreply.github.com>
2020-12-02 22:00:53 -0400

    ready validaciones del registro

--- src/app/Components/registro/registro.component.ts
+++ src/app/Components/registro/registro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/Servicios/users.service';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
 
@@ -17,14 +17,15 @@ import { AuthService } from 'src/app/auth/services/auth.service';
 
 export class RegistroComponent implements OnInit {
 
+  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   registroForm = new FormGroup({
-    usuario: new FormControl(''),
-    nombre: new FormControl(''),
-    contrasena: new FormControl(''),
-    contrasena2: new FormControl(''),
-    email: new FormControl(''),
-    telefono: new FormControl(''),
-    direccion: new FormControl(''),
+    usuario: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    nombre: new FormControl('',[Validators.required, Validators.minLength(6)]),
+    contrasena: new FormControl('',[Validators.required, Validators.minLength(6)]),
+    email: new FormControl('',[Validators.required, Validators.minLength(11), Validators.pattern(this.emailPattern)]),
+    telefono: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    direccion: new FormControl('', [Validators.required, Validators.minLength(10)]),
   });
 
  
@@ -37,20 +38,57 @@ export class RegistroComponent implements OnInit {
   }
 
   async onRegistro(){
-    const {email, contrasena}=this.registroForm.value;
 
-    try{
-      const user= await this.authSvc.registro(email,contrasena);
-      if(user){
-        this.router.navigate(['/vista'])
+    if(this.registroForm.valid){
+
+      const {email, contrasena}=this.registroForm.value;
+  
+      try{
+        const user= await this.authSvc.registro(email,contrasena);
+        if(user){
+          this.router.navigate(['/vista'])
+        }
+  
       }
+      catch(error){console.log(error);}
 
-    }
-    catch(error){console.log(error);}
+}else{
+
+  this.alertaU();
+}
    
   }
 
- 
+  alertaU(){
+    alert("Algun campo no es correcto, por favor vuelva a intentarlo")
+  }
 
-  
+get usuario() {
+  return this.registroForm.get('usuario')
 }
+get nombre() {
+  return this.registroForm.get('nombre')
+}
+get contrasena() {
+  return this.registroForm.get('contrasena')
+}
+get email() {
+  return this.registroForm.get('email')
+}
+get telefono() {
+  return this.registroForm.get('telefono')
+}
+get direccion() {
+  return this.registroForm.get('direccion')
+}
+
+password(){
+  const p = document.getElementById('password')
+  if (p.type === "password") {
+    p.type = "text";
+  } else {
+  p.type = "password";
+  }
+}
+
+ }
